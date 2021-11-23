aeddff1bea47fd386a73281ebfaeeda0c994c05b
TrexSamar <75522798+TrexSamar@users.noreply.github.com>
2021-11-23 16:51:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyBSLgnyX-QdsuIew4ZHDoQ8AzXckTh8_fQ",
+    authDomain: "fruit-catcher-a534a.firebaseapp.com",
+    databaseURL: "https://fruit-catcher-a534a-default-rtdb.firebaseio.com",
+    projectId: "fruit-catcher-a534a",
+    storageBucket: "fruit-catcher-a534a.appspot.com",
+    messagingSenderId: "668410094838",
+    appId: "1:668410094838:web:b2f96e6ab1f49a140138d7",
+    measurementId: "G-2X4ER9PJPR"
+  };
